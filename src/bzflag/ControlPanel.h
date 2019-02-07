89788ecdac2f71d73979d381661b6e4f5d94587a
Alfredo Tupone <atupone@gmail.com>
2019-02-07 08:05:06 +0100

    Remove glBegin/glEnd from ControlPanel

--- src/bzflag/ControlPanel.h
+++ src/bzflag/ControlPanel.h
@@ -24,6 +24,7 @@
 
 //common headers
 #include "bzfgl.h"
+#include "Vertex_Chunk.h"
 
 // local headers
 #include "MainWindow.h"
@@ -90,6 +91,8 @@ private:
     static void     exposeCallback(void*);
     static void     bzdbCallback(const std::string& name, void* data);
 
+    void computeConsoleOutline();
+
     enum MessageModes
     {
         MessageAllTabs = -2,
@@ -130,7 +133,8 @@ private:
     float       lineHeight;
     bool        unRead[MessageModeCount];
     bool        paused;
-
+    Vertex_Chunk consoleOutline;
+    bool         prevTabsOnRight;
 };
 
 inline void ControlPanel::setDimming(float newDimming)
