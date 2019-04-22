6db76550c8d8a5e8879a6f30e5bad64e129d5a75
Alfredo Tupone <atupone@gmail.com>
2019-04-22 09:45:21 +0200

    add OpenGLCommon class with some GL interfaces

--- src/ogl/OpenGLCommon.cxx
+++ src/ogl/OpenGLCommon.cxx
@@ -18,18 +18,20 @@
 // Common headers
 #include "bzfgl.h"
 
-void OpenGLCommon::getFogColor(glm::vec4 &fogColor)
+namespace OpenGLCommon
+{
+void getFogColor(glm::vec4 &fogColor)
 {
     glGetFloatv(GL_FOG_COLOR, glm::value_ptr(fogColor));
 }
 
-void OpenGLCommon::setFogColor(const glm::vec4 &fogColor)
+void setFogColor(const glm::vec4 &fogColor)
 {
     glFogfv(GL_FOG_COLOR, glm::value_ptr(fogColor));
 }
 
-void OpenGLCommon::setEyePlanes(const glm::vec4 &sPlane,
-                                const glm::vec4 &tPlane)
+void setEyePlanes(const glm::vec4 &sPlane,
+                  const glm::vec4 &tPlane)
 {
     glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR);
     glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR);
@@ -37,9 +39,44 @@ void OpenGLCommon::setEyePlanes(const glm::vec4 &sPlane,
     glTexGenfv(GL_T, GL_EYE_PLANE, glm::value_ptr(tPlane));
 }
 
-void OpenGLCommon::getViewPort(glm::ivec4 &viewport)
+void ClipPlane(int id, glm::vec4 plane)
+{
+    const GLdouble myPlane[] = { plane[0], plane[1], plane[2], plane[3]};
+    glClipPlane(GL_CLIP_PLANE0 + id, myPlane);
+}
+
+void Ortho(float left, float right, float bottom, float top, float nearVal, float farVal)
+{
+    glLoadIdentity();
+    glOrtho(left, right, bottom, top, nearVal, farVal);
+}
+
+void ClearDepth()
+{
+    glClearDepth(1.0);
+}
+
+void DepthRange(float depthRange, float depthRangeSize)
+{
+    GLclampd x_near = (GLclampd)depthRange * depthRangeSize;
+    glDepthRange(x_near, x_near + depthRangeSize);
+}
+
+void LightModelLocalViewer(bool enable)
+{
+    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, enable ? GL_TRUE : GL_FALSE);
+}
+
+void LightModelSpecular(bool enable)
+{
+    const GLint model = enable ? GL_SEPARATE_SPECULAR_COLOR : GL_SINGLE_COLOR;
+    glLightModeli(GL_LIGHT_MODEL_COLOR_CONTROL, model);
+}
+
+void getViewPort(glm::ivec4 &viewport)
 {
     glGetIntegerv(GL_VIEWPORT, glm::value_ptr(viewport));
+}
 }
 
 
