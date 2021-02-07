4c2465a915040120ee97f5c424cb4459570246d4
Alfredo Tupone <atupone@gmail.com>
2021-02-07 17:29:25 +0100

    use glMultiTexCoord4 and glNormal3f only

--- include/SceneNode.h
+++ include/SceneNode.h
@@ -128,17 +128,17 @@ public:
 
     static void     glTexCoord2fv(const GLfloat *coord)
     {
-        ::glTexCoord2fv(coord);
+        ::glMultiTexCoord4f(GL_TEXTURE0, coord[0], coord[1], 0, 1);
     };
 
     static void     glTexCoord2fv(const glm::vec2 &coord)
     {
-        ::glTexCoord2f(coord.s, coord.t);
+        ::glMultiTexCoord4f(GL_TEXTURE0, coord.s, coord.t, 0, 1);
     };
 
     static void     glNormal3fv(const GLfloat *normal)
     {
-        ::glNormal3fv(normal);
+        ::glNormal3f(normal[0], normal[1], normal[2]);
     };
 
     static void     glNormal3fv(const glm::vec3 &normal)
