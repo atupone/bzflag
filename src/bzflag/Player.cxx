640dfaa88f86f5ac74eaceac42b3f0724a046935
Alfredo Tupone <atupone@gmail.com>
2024-03-02 22:49:02 +0100

    lowest quality 2

--- src/bzflag/Player.cxx
+++ src/bzflag/Player.cxx
@@ -55,6 +55,7 @@ Player::Player(const PlayerId& _id, TeamColor _team,
     registered(false),
     verified(false),
     playerList(false),
+    pausedSphere(nullptr),
     lastVisualTeam(NoTeam),
     team(_team),
     type(_type),
@@ -109,10 +110,9 @@ Player::Player(const PlayerId& _id, TeamColor _team,
         tankIDLNode = new TankIDLSceneNode(tankNode);
         changeTeam(team);
         const float sphereRad = (1.5f * BZDBCache::tankRadius);
-        if (RENDERER.useQuality() >= 2)
+        {
             pausedSphere = new SphereLodSceneNode(state.pos, sphereRad);
-        else
-            pausedSphere = new SphereBspSceneNode(state.pos, sphereRad);
+        }
         pausedSphere->setColor(0.0f, 0.0f, 0.0f, 0.5f);
     }
 
