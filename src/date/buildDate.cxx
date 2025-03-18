2d648c443274d74398291cc4f6811f9f18490caf
Scott Wichser <blast007@users.sourceforge.net>
2025-03-17 19:16:10 -0500

    Update version to 2.4.31.

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -48,12 +48,12 @@
 #endif
 
 #ifndef BZ_REV
-#  define BZ_REV        30
+#  define BZ_REV        31
 #endif
 
 // DEVEL | RC# | STABLE | MAINT
 #ifndef BZ_BUILD_TYPE
-#  define BZ_BUILD_TYPE     "MAINT"
+#  define BZ_BUILD_TYPE     "DEVEL"
 #endif
 
 const char *bzfcopyright = "Copyright (c) 1993-2025 Tim Riker";
